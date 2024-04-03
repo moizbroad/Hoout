46e5ded08ce69471ee89d33220c443e01625a139
moizbroad <moizbroadthinker543@gmail.com>
2024-04-03 14:36:07 +0500

    design custom input field

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import InputField from "../components/Common/InputField";
 
 const Signin = () => {
   return (
-    <div>Signin</div>
-  )
-}
+    <>
+      <div>Signin</div>
+      <div className="px-12">
+        <InputField placeholder="djhqhjeb" />
+      </div>
+    </>
+  );
+};
 
-export default Signin
+export default Signin;
---
+++ src/components/Common/InputField.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const InputField = (props) => {
+  const {
+    id,
+    label,
+    type,
+    autoComplete,
+    required,
+    value,
+    defaultValue,
+    onBlur,
+    onKeyUp,
+    onKeyDown,
+    autoFocus,
+    disabled,
+    placeholder,
+    onChange,
+    minLength,
+    maxLength,
+    max,
+    pattern,
+    title,
+    marginTop,
+    marginBottom,
+    name,
+    icon: IconComponent,
+  } = props;
+
+  return (
+    <div className={`${marginTop && "mt-4"} ${marginBottom && "mb-4"} `}>
+      {label && (
+        <label
+          htmlFor={id}
+          className="block text-sm font-medium text-gray-700 pb-1.5"
+        >
+          {label}
+        </label>
+      )}
+
+      <div className="relative flex items-center">
+        {IconComponent && (
+          <span className="absolute left-3">
+            <IconComponent className="text-gray-400 h-5 w-5" />
+          </span>
+        )}
+        <input
+          className={`block w-full appearance-none  ${
+            IconComponent ? "pl-10" : ""
+          } rounded-md   py-3 px-3 outline-none  border border-[#D9D9D9] focus:outline-none sm:text-sm input-field`}
+          id={id}
+          label={label}
+          type={type}
+          max={max}
+          name={name}
+          minLength={minLength}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          pattern={pattern}
+          title={title}
+          value={value}
+          onBlur={onBlur}
+          onKeyDown={onKeyDown}
+          onKeyUp={onKeyUp}
+          autoFocus={autoFocus}
+          disabled={disabled}
+          defaultValue={defaultValue}
+          onChange={onChange}
+          autoComplete={autoComplete}
+          required={required}
+        
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InputField;

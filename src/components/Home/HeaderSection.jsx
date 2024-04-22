0f9fd2e6a559785363d03e53f9b01cf7fef8d69a
moizbroad <moizbroadthinker543@gmail.com>
2024-04-22 13:27:18 +0500

    add toggle condation

--- src/components/Home/HeaderSection.jsx
+++ src/components/Home/HeaderSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import cart from "../../assets/HeaderAndFooter/cart.svg";
 import heart from "../../assets/HeaderAndFooter/heart.svg";
 import persons from "../../assets/HeaderAndFooter/persons.svg";
@@ -7,15 +7,35 @@ import search from "../../assets/HeaderAndFooter/searchh.svg";
 import { useNavigate } from "react-router-dom";
 
 const HeaderSection = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
   const navigate = useNavigate();
   return (
     <section className="px-[10px] sm:px-[20px] md:px-[30px] lg:px-[30px] xl:px-[100px] flex-between py-[27px] bg-[#E9E6D6] ">
-      <div className="menu-cons">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-          </svg>
+      <div className="menu-cons xl:hidden" onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          />
+        </svg>
       </div>
-      <section className="navbar active flex-wrap flex flex-col lg:flex-row xl:flex-row gap-[20px] xl:gap-[20px]  items-start lg:items-center xl:items-center px-6 lg:px-0 xl:px-0">
+      <section
+        className={`navbar ${
+          isActive ? "active open-sidebar" : ""
+        }  flex-wrap flex flex-col lg:flex-row xl:flex-row gap-[20px] xl:gap-[20px]  items-start lg:items-center xl:items-center px-6 lg:px-0 xl:px-0`}
+      >
         <div>Home</div>
         <div>Values</div>
         <div>Product Range</div>
@@ -24,9 +44,20 @@ const HeaderSection = () => {
         <div className="header-logo flex items-center justify-between w-full lg:w-auto xl:w-auto pl-6 pr-6 md:pr-0">
           {" "}
           <img src={headerImage} />
-          <div className="close-menu">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          <div className="close-menu xl:hidden" onClick={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
             </svg>
           </div>
         </div>
